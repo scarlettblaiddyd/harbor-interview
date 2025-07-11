b7205da17ee996490fd33947ae4768aad201f6ce
scarlettblaiddyd <scarlett@void.dev>
2025-07-10 18:16:25 -0700

    Show nearest deadline when adding new dates

--- src/frontend/src/components/CalendarComponent.vue
+++ src/frontend/src/components/CalendarComponent.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue'
+import { ref, computed, watch, nextTick } from 'vue'
 import FullCalender from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import { useEventStore } from '@/stores/eventStore'
 import EventTooltip from './EventTooltip.vue'
+import { type components } from '../models'
+
+type ExtractedEvent = components["schemas"]["ExtractedEvent"]
 
 const eventStore = useEventStore()
 
@@ -13,10 +18,24 @@ const tooltipTimeout = ref<number | null>()
 const tooltipX = ref<number>(0)
 const tooltipY = ref<number>(0)
 
+const closestEvent = ref<ExtractedEvent | null>()
+const calendarRef = ref<any>()
+const remountCalendarKey = ref<number>(0)
+
 const calendarOptions = computed(() => (
   {
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
+    views: {
+      dayGridMonth: { buttonText: 'Month' },
+      timeGridWeek: { buttonText: 'Week' },
+      timeGridDay: { buttonText: 'Day' }
+    },
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
     weekends: true,
     // Map events from backend to calendar widget events
     events: eventStore.events.map(e => {
@@ -24,9 +43,28 @@ const calendarOptions = computed(() => (
         start: new Date(e.date).toISOString(),
         allDay: true,
         title: e.title,
-        extendedProps: { original: e } // Store event data in extended props for ease of access
+        extendedProps: { original: e }, // Store event data in extended props for ease of access
+        classNames: closestEvent.value?.title === e.title ? ['highlighted-event'] : []
       }
     }),
+    dayCellDidMount(info: any) {
+      const targetDate = closestEvent.value?.date
+      console.log(targetDate)
+      if (!targetDate) return
+
+      const eventDateStr = new Date(targetDate).toDateString()
+      const cellDateStr = info.date.toDateString()
+
+      if (eventDateStr === cellDateStr) {
+        // Add text to the cell
+        const label = document.createElement('div')
+        label.innerHTML = 'Nearest deadline!'
+        label.classList.add('bg-rose-400', 'text-md', 'text-white', '!mt-[-1.5em]', 'z-100', 'rounded-md')
+        info.el.appendChild(label)
+
+        info.el.classList.add('bg-blue-400', 'text-white')
+      }
+    },
     eventMouseEnter(info: any) {
       // Remove hide-tooltip timer so it doesn't dissapear while hovering
       if(tooltipTimeout.value) clearTimeout(tooltipTimeout.value)
@@ -60,11 +98,32 @@ watch(isHoveringTooltip, (value: boolean, _oldValue: boolean) => {
     tooltipVisible.value = false
   }
 })
+
+watch(() => eventStore.events, (newVal: ExtractedEvent[], _oldValue: any[]) => {
+  console.log('here')
+  console.log(newVal)
+  const now = new Date()
+  console.log(now)
+
+  const futureEvents = newVal.filter(e => new Date(e.date).getTime() > now.getTime())
+
+  if(!futureEvents.length) {
+    alert('All dates extracted are in the past, hope you didn\'t miss anything important!')
+  }
+
+  futureEvents.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+  closestEvent.value = futureEvents[0]
+  if(!calendarRef.value) return
+  const calendarApi = calendarRef.value.getApi()
+  calendarApi.gotoDate(new Date(closestEvent.value.date).toISOString())
+  // render extra cell styling for near deadlines
+  remountCalendarKey.value++
+}, {deep: true})
 </script>
 
 
 <template>
-  <FullCalender :options='calendarOptions' class="min-w-md w-full max-w-6xl"/>
+  <FullCalender ref="calendarRef" :key="remountCalendarKey" :options='calendarOptions' class="min-w-md w-full max-w-6xl"/>
   <EventTooltip
     :visible="tooltipVisible || isHoveringTooltip"
     :x="tooltipX"
@@ -74,3 +133,13 @@ watch(isHoveringTooltip, (value: boolean, _oldValue: boolean) => {
   />
 
 </template>
+
+
+<style lang="postcss">
+.highlighted-event {
+  background-color: rgb(131, 121, 29);
+  border-radius: 6px;
+  font-weight: bold;
+  border: 1px solid orange;
+}
+</style>
